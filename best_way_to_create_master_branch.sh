c6d1f459b53eb6e4cbd8e06b0d0a62aa507898c7
Bianca Rosa <me@biancarosa.com.br>
2016-02-01 12:33:06 -0200

    Add new commands

--- best_way_to_create_master_branch.sh
+++ best_way_to_create_master_branch.sh
@@ -3,3 +3,5 @@ git push origin --delete master
 git subtree split --prefix=app -b master
 git checkout master
 git push origin master
+git branch --set-upstream-to=origin/master master
+git checkout development
